e68934bfe221be6af38ccb558d0ad877ec01fd82
Dmitriy Yermolenko <dmitriy.yermolenko@revjet.com>
2019-11-28 06:30:29 +0200

    currency-exchange

---
+++ currency-exchange/precache-manifest.2a5f853fc08166b732f3928e2a2a1693.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf92ea6c336b0783e3cb823493c6361",
+    "url": "/currency-exchange/index.html"
+  },
+  {
+    "revision": "0f59413c4a65afe8ea4f",
+    "url": "/currency-exchange/static/css/main.03f1f571.chunk.css"
+  },
+  {
+    "revision": "ed2be3194492ed77bf60",
+    "url": "/currency-exchange/static/js/2.11fae78c.chunk.js"
+  },
+  {
+    "revision": "0f59413c4a65afe8ea4f",
+    "url": "/currency-exchange/static/js/main.94dafbd6.chunk.js"
+  },
+  {
+    "revision": "09a0f7784f0c1d818c71",
+    "url": "/currency-exchange/static/js/runtime-main.ade7e3dc.js"
+  }
+]);
